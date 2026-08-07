787cd719687ccdb191b96bdb0cb7a4e405a6cec7
Pavlo <pavlokudil.dev@gmail.com>
2026-08-07 10:48:13 +0200

    First upload
    Not a working version now

---
+++ http_lan_server.py
@@ -0,0 +1,103 @@
+import socket
+import re#GET /UpdatPR HTTP/1.1
+import json
+
+#create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.178.27", 80))
+
+def get_all_data(client_socket):   
+    print("\n===Receiving data from client...===\n")
+    data = b""
+    while b"\r\n\r\n" not in data:
+        data = data + client_socket.recv(1024)
+    print(f"===Received data from client:\n{data.decode('utf-8')}===\n")
+    return data.decode("utf-8")
+
+def ifcss(data, cl_socket):
+    if "style.css" in data:
+        with open("style.css", "r", encoding="utf-8") as f:
+            cl_socket.sendall((f"HTTP/1.1 200 OK\r\nContent-Type: text/css\r\n\r\n{f.read()}").encode("utf-8"))
+            print(f"\n===Sent style.css to {client_address}===\n")
+            return 1
+    return 0
+
+def iffavicon(data, cl_socket):
+    print("\n===Checking for favicon.ico request...===\n")
+    if "favicon.ico" in data:
+        cl_socket.sendall(b"HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nFavicon not found.")
+        print(f"\n===Sent favicon.ico not found message to {client_address}===\n")
+        return 1
+    return 0
+
+def separate_request(data):
+    request = re.findall("[POSTGET]+ /(.*?) HTTP/1.1\r\n", data)
+    print(f"\n===Separated request: {request}===\n")
+    if len(request) > 0:
+        return request[0]
+    return None
+
+def separate_body(data):
+    body = re.findall("\r\n\r\n(.*)", data)
+    print(f"\n===Separated body: {body}===\n")
+    if len(body) > 0:
+        return body[0]
+    return None
+
+def body_to_object(body):
+    infos = body.split("&")
+    dicks = {}
+    for info in infos:
+        key, value = info.split("=")
+        dicks[key] = value
+    return dicks
+
+def json_file_to_object():
+    with open("database.txt", "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def python_object_to_json_file(oby):
+    text = json.dumps(oby)
+    with open("database.txt", "w", encoding="utf-8") as f:
+        f.seek(0)
+        f.write(text)
+
+s.listen(1)
+
+active_users = []
+while True:
+    client_socket, client_address = s.accept()
+    try:
+        client_socket.settimeout(1.0)
+        print(f"Connection from {client_address} has been established!")
+        c_data = get_all_data(client_socket)
+
+        if ifcss(c_data, client_socket) == 0 and iffavicon(c_data, client_socket) == 0:
+            print(f"Received data:\n{c_data}")        
+            request = separate_request(c_data)
+            if not request:
+                with open("Login.html", "r", encoding="utf-8") as f:
+                    content = f.read()
+                    client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{content}".encode("utf-8"))
+                client_socket.close()
+                continue
+
+            if request == "add-pr" or request == "login" or request == "update-profile":
+                body = separate_body(c_data)
+                if body:
+                    oby_body = body_to_object(body)
+                    print(f"===Parsed JSON body: {oby_body}===\n")
+                    database = json_file_to_object()
+                    database.append(oby_body)
+                    python_object_to_json_file(database)
+            
+            client_socket.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!")
+        
+        client_socket.close()
+
+    except socket.timeout:
+        print(f"Connection from {client_address} timed out.")
+        client_socket.close()
+
+print("Server stopped.")
+s.close()
