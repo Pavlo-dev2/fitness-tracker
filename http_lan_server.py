5c28d0e73ea14f97b2d7f49ac2284338487799fa
Pavlo <pavlokudil.dev@gmail.com>
2026-08-10 09:38:04 +0200

    Add some login logik
    Need to work on login/create acount/database logik

--- http_lan_server.py
+++ http_lan_server.py
@@ -1,13 +1,14 @@
 import socket
 import re#GET /UpdatPR HTTP/1.1
 import json
-impot time
+import time
 
 #create a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.178.27", 80))
 
-def get_all_data(client_socket):   
+#get all data from the client
+def get_all_data(client_socket):
     print("\n===Receiving data from client...===\n")
     data = b""
     while b"\r\n\r\n" not in data:
@@ -15,6 +16,7 @@ def get_all_data(client_socket):
     print(f"===Received data from client:\n{data.decode('utf-8')}===\n")
     return data.decode("utf-8")
 
+#check if the request is for style.css and send it if it is
 def ifcss(data, cl_socket):
     if "style.css" in data:
         with open("style.css", "r", encoding="utf-8") as f:
@@ -23,6 +25,7 @@ def ifcss(data, cl_socket):
             return 1
     return 0
 
+#check if the request is for favicon.ico and send a 404 response if it is
 def iffavicon(data, cl_socket):
     print("\n===Checking for favicon.ico request...===\n")
     if "favicon.ico" in data:
@@ -31,6 +34,7 @@ def iffavicon(data, cl_socket):
         return 1
     return 0
 
+#separate the request from the data
 def separate_request(data):
     request = re.findall("[POSTGET]+ /(.*?) HTTP/1.1\r\n", data)
     print(f"\n===Separated request: {request}===\n")
@@ -38,6 +42,7 @@ def separate_request(data):
         return request[0]
     return None
 
+#separate the body from the data
 def separate_body(data):
     body = re.findall("\r\n\r\n(.*)", data)
     print(f"\n===Separated body: {body}===\n")
@@ -45,6 +50,7 @@ def separate_body(data):
         return body[0]
     return None
 
+#convert the body to a python object
 def body_to_object(body):
     infos = body.split("&")
     dicks = {}
@@ -53,19 +59,48 @@ def body_to_object(body):
         dicks[key] = value
     return dicks
 
+#convert the json file to a python object
 def json_file_to_object():
     with open("database.txt", "r", encoding="utf-8") as f:
         return json.load(f)
 
+#convert a python object to a json file
 def python_object_to_json_file(oby):
     text = json.dumps(oby)
     with open("database.txt", "w", encoding="utf-8") as f:
         f.seek(0)
         f.write(text)
 
+#check if user is in database
+def check_user_in_database(username, password):
+    with open("database.txt", "r", encoding="utf-8") as f:
+        database = json.load(f)
+        for user in database:
+            if user.get("username") == username:
+                if user.get("password") == password:
+                    return True
+                break
+    return False
+
+def check_user_exists(username):
+    with open("database.txt", "r", encoding="utf-8") as f:
+        database = json.load(f)
+        for user in database:
+            if user.get("username") == username:
+                return True
+    return False
+
+def send_html_response(client_socket, html_file):
+    with open(html_file, "r", encoding="utf-8") as f:
+        content = f.read()
+        client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{content}".encode("utf-8"))
+        client_socket.close()
+
 s.listen(1)
 
+#list to keep track of active users [client_address, username, status, timestamp]
 active_users = []
+
 while True:
     client_socket, client_address = s.accept()
     try:
@@ -76,40 +111,56 @@ while True:
         if ifcss(c_data, client_socket) == 0 and iffavicon(c_data, client_socket) == 0:
             print(f"Received data:\n{c_data}")        
             request = separate_request(c_data)
-            if not request:
-                with open("Login.html", "r", encoding="utf-8") as f:
-                    content = f.read()
-                    client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{content}".encode("utf-8"))
-                client_socket.close()
-                continue
 
-            if request == "add-pr" or request == "login" or request == "update-profile":
+            #Handle the request with no body, like login or create account, and send the appropriate page
+
+            #send the login page if there is no request
+            if not request:
+                send_html_response(client_socket, "Login.html")
+                continue
+            elif request == "create-account":#send the create account page
+                # Handle registration logic here
+                send_html_response(client_socket, "CreateAcount.html")
+            
+            #process the request
+            if request == "add-pr" or request == "login" or request == "update-profile" or request == "register":
                 body = separate_body(c_data)
                 if body:
                     try:
-                        oby_body = body_to_object(body)
+                        oby_body = body_to_object(body)#request body as python object
+
+
+
                         #print(f"===Parsed JSON body: {oby_body}===\n")
                         #database = json_file_to_object()
                         #database.append(oby_body)
                         #python_object_to_json_file(database)
+                        
+                        
+                        
+                        #process the request based on the request type
                         if request == "login":
                             # Handle login logic here
                             with open("database.txt", "r", encoding="utf-8") as f:
-                                user_found = False
-                                database = json.load(f)
-                                for user in database:
-                                    if user.get("username") == oby_body.get("username") and user.get("password") == oby_body.get("password"):
-                                        active_users.append([client_address, user.get("username"), "loged_in", time.time()])
-                                        with open("home.html", "r", encoding="utf-8") as home_file:
-                                            home_content = home_file.read()
-                                            client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{home_content}".encode("utf-8"))
-                                        user_found = True
-                                        break
-                                if not user_found:
-                                    active_users.append([client_address, None, "loging_in", time.time()])
-                                    with open("UpdateProfile.html", "r", encoding="utf-8") as update_file:
-                                            update_content = update_file.read()
-                                            client_socket.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{update_content}".encode("utf-8"))
+
+                                database = json.load(f)#database as python object
+
+                                if check_user_in_database(oby_body.get("username"), oby_body.get("password")) == True:
+                                    active_users.append([client_address, oby_body.get("username"), "loged_in", time.time()])
+                                    send_html_response(client_socket, "home.html")
+
+                                elif check_user_in_database(oby_body.get("username"), oby_body.get("password")) == False:
+                                    active_users.append([client_address, oby_body.get("username"), "wrong_login_data", time.time()])
+                                    send_html_response(client_socket, "WPLogin.html")
+
+                        elif request == "register":#register the user, handel the data fo new user and save it to the database
+                            if not check_user_exists(oby_body.get("username")):
+                                database = json_file_to_object()
+                                database.append(oby_body)
+                                python_object_to_json_file(database)
+                                send_html_response(client_socket, "home.html")
+                            else:
+                                send_html_response(client_socket, "Login.html")#user already exists, send them to the login page
 
                     except json.JSONDecodeError:
                         print("Error: Invalid JSON in request body.")
